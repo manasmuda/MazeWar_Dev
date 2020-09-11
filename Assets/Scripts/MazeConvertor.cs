a6fb6eab12f472a75dc5d2884d81c4e2ff7f21e5
manas <manasgupta435@gmail.com>
2020-09-11 23:46:06 +0530

    gameready state added and some character material changed

---
+++ Assets/Scripts/MazeConvertor.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System;
+
+public class MazeConvertor 
+{
+    public static MazeCell[,] ToMazeArray(List<object> list)
+    {
+        MazeCell[,] maze = new MazeCell[20, 20];
+        for(int x = 0; x<list.Count; x++)
+        {
+            int i = x / 20;
+            int j = x % 20;
+            string temp=Convert.ToString(list[x]);
+            string[] temparr = temp.Split(',');
+            maze[i, j] = new MazeCell(i,j);
+            if (temparr[0] == "0")
+            {
+                maze[i, j].northWall = false;
+            }
+            if (temparr[1] == "0")
+            {
+                maze[i, j].southWall = false;
+            }
+            if (temparr[2] == "0")
+            {
+                maze[i, j].eastWall = false;
+            }
+            if (temparr[3] == "0")
+            {
+                maze[i, j].westWall = false;
+            }
+        }
+        return maze;
+    }
+}
