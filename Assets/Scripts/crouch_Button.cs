0152a465171136620188652c1fe9002ba896924f
mohamedibrahim155 <ibrahim155.developer@gmail.com>
2020-09-16 13:20:30 +0530

    made changes in character controller  update crouch script

---
+++ Assets/Scripts/crouch_Button.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.EventSystems;
+
+public class crouch_Button : MonoBehaviour, IPointerClickHandler
+{
+    public bool isCrouched;
+    public int hitTimes = 0;
+    public static crouch_Button instance;
+
+    // Start is called before the first frame update
+    
+      
+    void Start()
+    {
+        if (instance == null)
+        {
+            instance = this;
+        }
+        else {
+            Destroy(gameObject);
+        }
+
+        isCrouched = false;
+    }
+   
+
+    public void OnPointerClick(PointerEventData eventData)
+    {
+        if (!isCrouched&& hitTimes ==0)
+        {
+            isCrouched = true;
+            hitTimes++;
+        }
+        else if (isCrouched&& hitTimes>0)
+        {
+            isCrouched = false;
+            hitTimes = 0;
+        }
+        
+    }
+}
