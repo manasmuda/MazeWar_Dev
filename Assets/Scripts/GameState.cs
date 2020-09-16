ca7030c5736deeba0b81ba5fe1a9307a6eb59c4d
Manas Muda <manasgupta435@gmail.com>
2020-09-16 12:48:22 +0530

    network sync bug fixes

---
+++ Assets/Scripts/GameState.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System;
+
+[Serializable]
+public class GameState 
+{
+    public int tick;
+    public int stateType;
+
+    public List<ClientState> blueTeamState = new List<ClientState>();
+    public List<ClientState> redTeamState = new List<ClientState>();
+
+}
