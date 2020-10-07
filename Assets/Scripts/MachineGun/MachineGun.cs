4b92bcb5920f957238d1a971e648819d85ae396b
mohamedibrahim155 <ibrahim155.developer@gmail.com>
2020-10-07 16:58:02 +0530

    AI turret

--- Assets/Scripts/MachineGun/MachineGun.cs
+++ Assets/Scripts/MachineGun/MachineGun.cs
@@ -9,56 +9,89 @@ public class MachineGun : MonoBehaviour
 
     [Header("overlap Circle")]
     public bool isEntered = false;
+    
     public float radius;
-    public LayerMask playerLayer;
-    public Vector3 Offset;
-
+    
     [Header("Target to attack")]
-    public Transform targetToAttack;
+
+  
+   public Transform targetToAttack = null;
+  //  public Transform currenttarget = null;
 
     [Header("Firing")]
     public bool canShoot = true;
+    private bool inSight = false;
+   
     public float fireRate = 0.5f;
     public float bulletForce = 100f;
-    public Transform bulletSpawnPosition;
+    public Transform  bulletSpawnPosition;
     public GameObject bulletPrefab;
+  
+    [SerializeField]
+    RaycastHit hit;
+    private Vector3 direction;
 
+  
     // Start is called before the first frame update
     void Start()
     {
         canShoot = true;
+       this.transform.GetComponent<SphereCollider>().radius = radius;
     }
 
     // Update is called once per frame
     void Update()
     {
-        isEntered = Physics.CheckSphere(transform.position +Offset, radius, playerLayer);
-        if (isEntered)
-        {
-            Debug.Log("Player entered the radius");
-            Vector3 direction = (new Vector3(targetToAttack.position.x,this.transform.position.y,targetToAttack.position.z) - this.transform.position).normalized;
-            Debug.DrawRay(this.transform.position, new Vector3(direction.x, 0f, direction.z), Color.white);
-            Quaternion lookRotation = Quaternion.LookRotation(new Vector3(direction.x, 0f, direction.z));
-            transform.rotation = Quaternion.Slerp(transform.rotation, lookRotation, Time.deltaTime * turnSpeed);
-            if (canShoot)
-            {
-                if (Vector3.Angle(direction, transform.forward) < 5f)
-                    Fire();
-                    canShoot = false;
-                    Invoke("ChangeFire", fireRate);
-            }
-           
 
 
-        }
-        else {
+        //direction = (new Vector3(targetToAttack.position.x, this.transform.position.y, targetToAttack.position.z) - this.transform.position);
 
-            Debug.Log("player Not entered and  looking for the player");
-        }
         
+        if (targetToAttack != null)
+        {
+           
+            if (inSight =Physics.Raycast(this.transform.position, direction, out hit) )
+            Debug.DrawRay(this.transform.position, direction, Color.white);
+            {
+                direction = (new Vector3(targetToAttack.position.x, this.transform.position.y, targetToAttack.position.z) - this.transform.position).normalized;
+                if (hit.collider != null)
+                {
+                    if (hit.collider.tag == "wall" || hit.collider.tag == "Maze")
+                    {
+                        if ( targetToAttack !=null)
+                        {
+                            targetToAttack = null;
+                        }
+
+                        
+                    }
+                    else
+                    {
+                        Quaternion lookRotation = Quaternion.LookRotation(new Vector3(direction.x, 0f, direction.z));
+                        this.transform.rotation = Quaternion.Slerp(this.transform.rotation, lookRotation, Time.deltaTime * turnSpeed);
+
+                        if (canShoot)
+                        {
+
+                            if (Vector3.Angle(direction, this.transform.forward) < 5f)
+                            {
+                                Fire();
+                            }
+
+                            canShoot = false;
+                            Invoke("ChangeFire", fireRate);
+                        }
+
+                    }
+
+
+                }
+            }
+
+        }
 
     }
-
+   
 
     void ChangeFire()
     {
@@ -67,16 +100,37 @@ public class MachineGun : MonoBehaviour
 
     void Fire()
     {
-        GameObject bullet = Instantiate(bulletPrefab, bulletSpawnPosition.position, Quaternion.identity);
+        
+       GameObject bullet = Instantiate(bulletPrefab, bulletSpawnPosition.position, Quaternion.identity);
         bullet.AddComponent<Rigidbody>();
         bullet.GetComponent<Rigidbody>().AddForce(bulletSpawnPosition.forward * bulletForce, ForceMode.Impulse);
     }
 
-    private void OnDrawGizmos()
+    
+
+    private void OnTriggerStay(Collider other)
     {
-        Gizmos.color = Color.red;
-        Gizmos.DrawWireSphere(transform.position + Offset, radius);
+        //if (other.CompareTag("Maze") == false && other.CompareTag("wall")== false && other.CompareTag("Bullet") )
+        {
+         Debug.Log(other.name + " collider");
+           if (targetToAttack == null)
+            {
+               
+             targetToAttack = other.gameObject.transform;
+               
+            
+            }
+            
+        }
+    }
+    private void OnTriggerExit(Collider other)
+    {
+        if (targetToAttack == other.gameObject.transform)
+        {
+            targetToAttack = null;
+           
+        }
+    }
 
-    }   
-
+  
 }
